d10b00b6d23e3e79b56cc48e578031c16dd2ae77
rareaqil <rareaqil123@gmail.com>
2024-02-23 08:53:08 +0700

    Initial commit

---
+++ database/seeders/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
